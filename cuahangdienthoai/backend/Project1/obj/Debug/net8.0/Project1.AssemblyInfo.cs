9f644060605681b8cb0a82bcbda1ea150c029412
Hoan <hoangmanhhoan04@gami.com>
2026-04-21 12:15:20 +0700

    fix: add ngrok-skip-browser-warning header

--- cuahangdienthoai/backend/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ cuahangdienthoai/backend/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8b35441226e457e686734b05b8ac05716ffd60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b83db4c20e0a8bd85106d0170798039a96e24e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- cuahangdienthoai/frontend/frontend/src/app/interceptors/auth.interceptor.ts
+++ cuahangdienthoai/frontend/frontend/src/app/interceptors/auth.interceptor.ts
@@ -3,13 +3,12 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-  }
+  const cloned = req.clone({
+    setHeaders: {
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      'ngrok-skip-browser-warning': 'true'
+    }
+  });
 
-  return next(req);
+  return next(cloned);
 };
